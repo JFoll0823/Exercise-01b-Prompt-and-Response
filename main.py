167c68b8f5ee91fc46c45d54048028b1c81b578e
JFoll0823 <justinfoll23@gmail.com>
2021-08-30 13:52:52 -0400

    Update main.py
    Added code to test user response to a prompted message.  Tested within file.

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
 
 passage_name = "West of House"
 passage_text = "This is an open field west of a white house, with a boarded front door."
+
+print("You are at the "+passage_name+"\n"+passage_text+"\nWhat would you like to do? ")
+response = input()
+if(response == "go north"):
+    print("Good job!")
+
+#if the user types "Go north" or "Go North" the message "Good job!" is not displayed because capitalization matters.
